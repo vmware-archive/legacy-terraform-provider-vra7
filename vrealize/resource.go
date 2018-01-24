3ec1fb7e0a4ddb6de75be839f52c79cecbdaef0c
Viraj Indasrao <virajindasrao@gmail.com>
2018-01-24 21:47:25 +0530

    #7 Terraform "depends_on" does not wait - wait_timeout resource schema added. (#10)
    * Terraform "depends_on" does not wait - wait_timeout resource schema added.
    * Add a depends_on example
    
    Signed-off-by: Viraj Indasrao <virajindasrao@gmail.com>

--- vrealize/resource.go
+++ vrealize/resource.go
@@ -111,6 +111,11 @@ func setResourceSchema() map[string]*schema.Schema {
 			Computed: true,
 			Optional: true,
 		},
+		"wait_timeout": {
+			Type:     schema.TypeInt,
+			Optional: true,
+			Default:  15,
+		},
 		"request_status": {
 			Type:     schema.TypeString,
 			Computed: true,
@@ -176,9 +181,7 @@ func createResource(d *schema.ResourceData, meta interface{}) error {
 
 	//If catalog name is provided then get catalog ID using name for further process
 	//else if catalog id is provided then fetch catalog name
-	log.Println("print before block")
 	if len(d.Get("catalog_name").(string)) > 0 {
-		log.Println("print in block")
 		catalogID, returnErr := client.readCatalogIDByName(d.Get("catalog_name").(string))
 		log.Printf("createResource->catalog_id %v\n", catalogID)
 		if returnErr != nil {
@@ -269,6 +272,35 @@ func createResource(d *schema.ResourceData, meta interface{}) error {
 	d.SetId(requestMachine.ID)
 	//Set request status
 	d.Set("request_status", "SUBMITTED")
+
+	waitTimeout := d.Get("wait_timeout").(int) * 60
+
+	for i := 0; i < waitTimeout/30; i++ {
+		time.Sleep(3e+10)
+		readResource(d, meta)
+
+		if d.Get("request_status") == "SUCCESSFUL" {
+			return nil
+		}
+		if d.Get("request_status") == "FAILED" {
+			//If request is failed during the time then
+			//unset resource details from state.
+			d.SetId("")
+			return fmt.Errorf("instance got failed while creating." +
+				" kindly check detail for more information")
+		}
+	}
+	if d.Get("request_status") == "IN_PROGRESS" {
+		//If request is in_progress state during the time then
+		//keep resource details in state files and throw an error
+		//so that the child resource won't go for create call.
+		//If execution gets timed-out and status is in progress
+		//then dependent machine won't be get created in this iteration.
+		//A user needs to ensure that the status should be a success state
+		//using terraform refresh command and hit terraform apply again.
+		return fmt.Errorf("resource is still being created")
+	}
+
 	return nil
 }
 
