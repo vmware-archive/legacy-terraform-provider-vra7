332d02a05563df6f1db92cc3f4f16a96c93f7fb0
Viraj Indasrao <virajindasrao@gmail.com>
2018-04-18 14:26:07 +0530

    Example with output and comments added Minor fixes Signed-off-by: Viraj Indasrao <virajindasrao@gmail.com>

--- example/get-deployment-details/main.tf
+++ example/get-deployment-details/main.tf
@@ -5,7 +5,6 @@ provider  "vra7" {
     host = "${var.host}"
 }
 
-<<<<<<< HEAD
 //TF resource with one centos deployment
 resource "vra7_resource" "CentOS_machine" {
   count            = 1
@@ -27,15 +26,3 @@ output "centOsMachineCPU" {
 output "WinMachineAddress" {
   value = "${vra7_resource.CentOS_machine.resource_configuration.Win.ip_address}"
 }
-=======
-resource "vra7_resource" "multi_machine_cl" {
-  count            = 1
-  catalog_name = "CentOS"
-  resource_configuration = {
-    Linux.0.cpu = ""
-    Linux.0.memory = ""
-    Linux.1.cpu = ""
-    Linux.1.memory = ""
-  }
-}
->>>>>>> d2f1a81... Fetch deployment details example
