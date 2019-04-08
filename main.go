083de474f15dabab18533d4623cabfd664aa172e
Prativa Bawri <bawrip@vmware.com>
2019-04-08 15:50:10 -0700

    Rename dirs/files according to the hashicorp provider's guidelines (#145)
    * Rename dirs/files according to the hashicorp providers' guidelines
    
    Signed-off-by: Prativa Bawri<bawrip@vmware.com>

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/vmware/terraform-provider-vra7/utils"
-	"github.com/vmware/terraform-provider-vra7/vrealize"
+	"github.com/vmware/terraform-provider-vra7/vra7"
 )
 
 func main() {
 	utils.InitLog()
 	opts := plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return vrealize.Provider()
+			return vra7.Provider()
 		},
 	}
 
--- vra7/resource_vra7_deployment_test.go
+++ vra7/resource_vra7_deployment_test.go
@@ -1,4 +1,4 @@
-package vrealize
+package vra7
 
 import (
 	"encoding/json"
